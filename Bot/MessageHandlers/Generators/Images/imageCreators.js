88047c83125126708ad7698207596ed6654ee684
jamesstudd <james.studd@live.co.uk>
2021-01-31 21:12:19 +0000

    Revert "Revert "Added command to get runescape stats and posts an image in chat, not the cleanest but works for now""
    This reverts commit 29263623670e4c6b5506cdea215acdcbceff9119.

--- Bot/MessageHandlers/Generators/Images/imageCreators.js
+++ Bot/MessageHandlers/Generators/Images/imageCreators.js
@@ -1,28 +1,70 @@
-import { createCanvas, loadImage } from "canvas";
+import { createCanvas, loadImage, registerFont } from "canvas";
+const path = require("path");
 
 const BASE_EMOJI_URL = "https://cdn.discordapp.com/emojis/";
 const BASE_TWEMOJI = "https://twemoji.maxcdn.com/v/latest/";
 const TWEMOJI_SIZE = "72x72/";
 const FILE_EXTENSION = ".png";
 
-async function DrawEmojis(emojis, size, context) {
-	for (const emoji of emojis) {
-		const x = emoji.overrides ? emoji.overrides[0] : 0;
-		const y = emoji.overrides ? emoji.overrides[1] : 0;
-		const width = emoji.overrides ? emoji.overrides[2] : size;
-		const height = emoji.overrides ? emoji.overrides[3] : size;
-		if (emoji.type == "custom") {
-			const img = await loadImage(
-				`${BASE_EMOJI_URL}${emoji.id}${FILE_EXTENSION}`
-			);
-			context.drawImage(img, x, y, width, height);
-		} else if (emoji.type == "inbuilt") {
-			const img = await loadImage(
-				`${BASE_TWEMOJI}${TWEMOJI_SIZE}${emoji.id}${FILE_EXTENSION}`
-			);
-			context.drawImage(img, x, y, width, height);
+let runescapeSkillsBase;
+let runescapeBasePositions = {
+	top: [90, 40],
+	bottom: [120, 70],
+};
+
+registerFont(path.join(__dirname, "Runescape", "RuneScape-Chat-07.ttf"), {
+	family: "Runescape",
+});
+
+(async () => {
+	runescapeSkillsBase = await loadImage(
+		path.join(__dirname, "Runescape", "Blank.png")
+	);
+})();
+
+export async function CreateRunescapeSkills(runescapeSkills) {
+	const width = 503;
+	const height = 681;
+	const canvas = createCanvas(width, height);
+	const context = canvas.getContext("2d");
+
+	context.drawImage(runescapeSkillsBase, 0, 0);
+
+	context.fillStyle = "yellow";
+	context.font = '32px "Runescape"';
+
+	let currOffsetDown = 0;
+	let currOffsetAcross = 0;
+
+	let offsetDown = 85;
+	let offsetAcross = 168;
+
+	context.fillText(runescapeSkills.skills.TotalLevel.level, 390, 665);
+
+	Object.keys(runescapeSkills.skills).forEach((skill, i) => {
+		if (i === 0) return;
+
+		let level = runescapeSkills.skills[skill].level;
+		let { top, bottom } = runescapeBasePositions;
+
+		let xTop = top[0] + currOffsetAcross;
+		let yTop = top[1] + currOffsetDown;
+
+		context.fillText(level, xTop, yTop);
+
+		let xBot = bottom[0] + currOffsetAcross;
+		let yBot = bottom[1] + currOffsetDown;
+
+		context.fillText(level, xBot, yBot);
+
+		currOffsetDown += offsetDown;
+		if (i != 0 && i % 8 === 0) {
+			currOffsetAcross += offsetAcross;
+			currOffsetDown = 0;
 		}
-	}
+	});
+
+	return canvas.toBuffer("image/png");
 }
 
 export async function CreateCombinedImage(emojis) {
@@ -65,6 +107,26 @@ export async function CreateMeme(topText, bottomText, emojis) {
 	});
 
 	return canvas.toBuffer("image/png");
+}
+
+async function DrawEmojis(emojis, size, context) {
+	for (const emoji of emojis) {
+		const x = emoji.overrides ? emoji.overrides[0] : 0;
+		const y = emoji.overrides ? emoji.overrides[1] : 0;
+		const width = emoji.overrides ? emoji.overrides[2] : size;
+		const height = emoji.overrides ? emoji.overrides[3] : size;
+		if (emoji.type == "custom") {
+			const img = await loadImage(
+				`${BASE_EMOJI_URL}${emoji.id}${FILE_EXTENSION}`
+			);
+			context.drawImage(img, x, y, width, height);
+		} else if (emoji.type == "inbuilt") {
+			const img = await loadImage(
+				`${BASE_TWEMOJI}${TWEMOJI_SIZE}${emoji.id}${FILE_EXTENSION}`
+			);
+			context.drawImage(img, x, y, width, height);
+		}
+	}
 }
 
 function FitText(text, context, size) {
