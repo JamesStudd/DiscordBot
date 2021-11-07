e3bdcf9a5e7c023327af62734ff8346758ccc237
JakeMLyons <79255211+JakeMLyons@users.noreply.github.com>
2021-11-07 00:01:08 +0000

    Create strike.js
    added a role command :)

---
+++ Bot/MessageHandlers/Commands/strike.js
@@ -0,0 +1,84 @@
+const ROLES = {
+    LUCKY : "906662243079454790",
+    STRIKEONE : "906662165333835866",
+    STRIKETWO : "906662214272958505",
+    BANNED : "906662334699806810",
+}
+
+function getMemberFromMention(mention, channel) {
+	if (mention.startsWith("<@") && mention.endsWith(">")) {
+		mention = mention.slice(2, -1);
+
+		if (mention.startsWith("!")) {
+            mention = mention.slice(1);
+			const foundMember = channel.members.find(
+                (member) => member.user.id === mention
+            );
+            return foundMember;
+		}
+
+        return undefined;
+	}
+}
+
+function addRoleById(msg, target, roleId) {
+    let role = msg.guild.roles.cache.find(r => r.id === roleId);
+    target.roles.add(role)
+        .then(() => {
+            console.log(`Given ${role} to <@${target.user.id}>`);
+        })
+        .catch(console.error);
+}
+
+function removeRoleById(msg, target, roleId) {
+    let role = msg.guild.roles.cache.find(r => r.id === roleId);
+    target.roles.remove(role)
+        .then(() => {
+            console.log(`Given ${role} to <@${target.user.id}>`);
+        })
+        .catch(console.error);
+}
+
+module.exports = {
+	name: "strike",
+	help: "Strike",
+	command: async function (msg, args) {
+        let body = args.join(" ");
+        let targetMember = getMemberFromMention(body, msg.channel);
+        if (!targetMember) {
+            msg.channel.send(`Could not find member: ${target}`);
+            return;
+        }
+
+        let targetId = targetMember.user.id;
+        let memberRoles = msg.member.roles.cache;
+
+        if (memberRoles.has(ROLES.LUCKY)) {
+            removeRoleById(msg, targetMember, ROLES.LUCKY);
+            msg.channel.send(`<@${targetId}> lucked out this time!`);
+            return;
+        }
+
+        if (memberRoles.has(ROLES.BANNED)) {
+            msg.channel.send(`<@${targetId}> is banned!`);
+            return;
+        }
+
+        if (memberRoles.has(ROLES.STRIKETWO)) {
+            removeRoleById(msg, targetMember, ROLES.STRIKETWO);
+            addRoleById(msg, targetMember, ROLES.BANNED);
+            msg.channel.send(`<@${targetId}> has been banned!`);
+            return;
+        }
+
+        if (memberRoles.has(ROLES.STRIKEONE)) {
+            removeRoleById(msg, targetMember, ROLES.STRIKEONE);
+            addRoleById(msg, targetMember, ROLES.STRIKETWO);
+            msg.channel.send(`<@${targetId}> is on their second strike!`);
+            return;
+        }
+
+        addRoleById(msg, targetMember, ROLES.STRIKEONE);
+        msg.channel.send(`<@${targetId}> is on their first strike!`);
+	},
+};
